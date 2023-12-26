46a898ec64a7e4a6d77f811e14805e13fa4cd73b
Shangharsh Adhikari <san.adh1996@gmail.com>
2023-12-26 17:12:56 +0545

    feat: UI of Quiz's Question and option

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,14 +1,60 @@
-import React, { useEffect } from 'react'
+import { CircularProgress } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import Question from '../components/Question/Question';
 
 const Quiz = ({name, questions, setQuestions, score, setScore}) => {
+  const [options, setOptions] = useState();
+  const [curQues, setCurQues] = useState(0);
 
-  useEffect(()=>{
-    console.log(questions);
-  },[questions]);
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[curQues]?.correct_answer,
+          ...questions[curQues]?.incorrect_answers,   
+        ])
+    );
+  }, [questions]);
 
+  console.log(questions);
+  
+  const handleShuffle = (optionItems) => {
+    return optionItems.sort(() => Math.random() - 0.5);
+  };
+  
   return (
-    <div>Quiz</div>
-  )
-}
+    <div>
+      <div className='text-center'>
+      <span className='subtitle text-center border border-primary p-2 rounded'>Welcome, {name}</span>
+      </div>
+      {questions?(
+        <>
+        <div className='d-flex justify-content-center align-items-center mt-3 flex-column'>
+          <div className='w-50 d-flex justify-content-between align-items-center'>
+            <p>Category: {questions[curQues].category}</p>
+            <p>Score: {score}</p>
+          </div>
+          
+        <Question 
+          curQues={curQues} 
+          setCurQues={setCurQues}
+          questions={questions}
+          options={options}
+          correct={questions[curQues]?.correct_answer}
+          score={score}
+          setScore={setScore}
+          setQuestions={setQuestions}
+          />
+        </div>
+        </>
+      ):( 
+      <div className='text-center'>
+        <CircularProgress 
+        className='mt-1'/>
+      </div> 
+      )}
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;
